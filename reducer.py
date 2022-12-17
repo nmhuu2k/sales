479df6157cacd0061afafbae4c79d8a206e9c5a2
Your Name <you@example.com>
2022-12-17 10:00:22 +0700

    asd

--- reducer.py
+++ reducer.py
@@ -7,12 +7,14 @@ current_word = None
 current_count = 0
 word = None
 
-# read the entire line from STDIN
+# input comes from STDIN
 for line in sys.stdin:
     # remove leading and trailing whitespace
     line = line.strip()
-    # splitting the data on the basis of tab we have provided in mapper.py
+
+    # parse the input we got from mapper.py
     word, count = line.split('\t', 1)
+
     # convert count (currently a string) to int
     try:
         count = int(count)
@@ -21,7 +23,7 @@ for line in sys.stdin:
         # ignore/discard this line
         continue
 
-    # this IF-switch only works because Hadoop sorts map output
+        # this IF-switch only works because Hadoop sorts map output
     # by key (here: word) before it is passed to the reducer
     if current_word == word:
         current_count += count
@@ -33,7 +35,7 @@ for line in sys.stdin:
         current_count = count
         current_word = word
 
-# do not forget to output the last word if needed!
+    # do not forget to output the last word if needed!
 if current_word == word:
     print
     '%s\t%s' % (current_word, current_count)
