ded1075a1cba4ffb864be3ba94e086693274a446
Your Name <you@example.com>
2022-12-17 09:32:30 +0700

    asd

--- reducer.py
+++ reducer.py
@@ -6,7 +6,7 @@ import sys
 current_word = None
 current_count = 0
 word = None
-# input comes from STDIN 
+# input comes from STDIN
 for line in sys.stdin:
     # remove leading and trailing whitespace
     line = line.strip()
@@ -20,7 +20,7 @@ for line in sys.stdin:
         # ignore/discard this line
         continue
     # this IF-switch only works because Hadoop sorts map output
-# by key (here: word) before it is passed to the reducer
+    # by key (here: word) before it is passed to the reducer
     if current_word == word:
         current_count += count
     else:
@@ -29,8 +29,8 @@ for line in sys.stdin:
             print
             '%s\t%s' % (current_word,
                         current_count)
-        current_count = count
-        current_word = word
-    # do not forget to output the last word if needed!
+            current_count = count
+            current_word = word
+# do not forget to output the last word if needed!
 if current_word == word:
     print('%s\t%s' % (current_word, current_count))
