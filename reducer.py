2286fbabc6c49a8d26e7d767197768f2e0d01efb
Your Name <you@example.com>
2022-12-17 09:30:26 +0700

    asd

--- reducer.py
+++ reducer.py
@@ -32,5 +32,5 @@ for line in sys.stdin:
         current_count = count
         current_word = word
     # do not forget to output the last word if needed!
-    if current_word == word:
-        print('%s\t%s' % (current_word, current_count))
+if current_word == word:
+    print('%s\t%s' % (current_word, current_count))
