7881f09e3d281a47c78af7c6368da75503cd455f
Your Name <you@example.com>
2022-12-17 09:21:44 +0700

    asd

--- reducer.py
+++ reducer.py
@@ -21,14 +21,14 @@ for line in sys.stdin:
         continue
     # this IF-switch only works because Hadoop sorts map output
 # by key (here: word) before it is passed to the reducer
-if current_word == word:
-    current_count += count
-else:
-    if current_word:
-        # write result to STDOUT
-        print
-        '%s\t%s' % (current_word,
-                    current_count)
+    if current_word == word:
+        current_count += count
+    else:
+        if current_word:
+            # write result to STDOUT
+            print
+            '%s\t%s' % (current_word,
+                        current_count)
 current_count = count
 current_word = word
 # do not forget to output the last word if needed! 
